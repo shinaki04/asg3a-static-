e6b108df312114abd65eabd6ae7343a19a518dfc
shinaki04 <shinak178@gmail.com>
2025-05-05 21:21:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require('http');  // Imports Node.js's built-in HTTP module to create a web server.
+const fs = require('fs');  // Imports the File System module to read files from disk 
+const path = require('path'); // //Imports the Path module to handle and normalize file paths across different operating systems.
 
 
 const server = http.createServer((req, res) => {
@@ -23,6 +23,12 @@ const server = http.createServer((req, res) => {
         }
     });
 });
+/*It hosts your frontend files (HTML, CSS, JS).
 
+It serves them to the browser when someone visits your site.
+
+It handles basic routing (like /, /app.js, etc.).
+
+It sets the right file type so the browser can render your website properly.*/
 
 server.listen(3000, () => console.log('Server running at http://localhost:3000/'));
